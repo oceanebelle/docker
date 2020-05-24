0ec1a1fe343b5f42e95d9924679fa0dd968d8d38
oceanebelle <oceanebelle@users.noreply.github.com>
2020-05-24 18:35:55 +0100

    undo some build

--- openjdk/build-docker-image.sh
+++ openjdk/build-docker-image.sh
@@ -6,4 +6,4 @@ set -e
 #docker buildx create --use --name localbuilder node-amd64
 #docker buildx create --append --name localbuilder node-arm64
 
-docker buildx build --platform linux/amd64,linux/arm/v7 --no-cache -t oceanebelle/openjdk:test --push .
+docker buildx build --platform linux/amd64,linux/arm/v7 --no-cache -t oceanebelle/openjdk:test --push -f Dockerfile-multi .
