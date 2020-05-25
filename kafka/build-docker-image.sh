997a9cdc9d7326d4f9d4419c5bac243408c16a6c
oceanebelle <oceanebelle@users.noreply.github.com>
2020-05-25 14:47:42 +0100

    add kafka images

--- kafka/build-docker-image.sh
+++ kafka/build-docker-image.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -e
 
-# build latest locally
-# docker build -t oceanebelle/zookeeper .
-
-# builds multiple architectures for the developer tag and upload to dockerhub
+# Images are built using github actions
 image=$(basename $PWD)
 docker buildx build --platform linux/arm/v7,linux/amd64 --pull --no-cache -t "oceanebelle/${image}:developer" --push -f Dockerfile.developer .
