1722aae7401d27968fa6d071f41ba3baf81d0a1b
oceanebelle <oceanebelle@users.noreply.github.com>
2021-04-08 22:06:33 +0100

    log cleanup

--- apps/cloudflared-sample/server.py
+++ apps/cloudflared-sample/server.py
@@ -1,14 +1,15 @@
 from flask import Flask
 import logging
 
+logger = logging.getLogger('waitress')
+logger.setLevel(logging.INFO)
+
 app = Flask(__name__)
 
 @app.route('/') 
-def hello_world(): 
-  logger = logging.getLogger('waitress')
-  logger.setLevel(logging.DEBUG)
-  logger.info("hello")
-  return "Hello, World! Oceanebelle! Serving on WSGI"
+def hello_world():   
+  logging.info("Hello world here.")
+  return "Hello, World! Oceanebelle! Serving on WSGI. With Logging"
 
 if __name__ == '__main__': 
   # waitress is a production WSGI server
